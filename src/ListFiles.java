1f2aecff2914dca8f575bdb436aa2e2cec84ed26
poromk <porom.kamal@gmail.com>
2022-06-01 18:28:30 -0400

    Remove filehash class, and organize code.

--- src/ListFiles.java
+++ src/ListFiles.java
@@ -1,27 +1,34 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 public class ListFiles {
-    public static void dirTree(File dir, HashMap<String, String> data) throws NoSuchAlgorithmException, IOException {
+    public static void dirTree(File dir, HashMap<String, String> data) throws NoSuchAlgorithmException, IOException, URISyntaxException {
         File[] subdirs = dir.listFiles();
-        FileWriter writer = null;
         for (File subdir : subdirs) {
             if (subdir.isDirectory()) {
                 dirTree(subdir, data);
             } else {
                 data.put(subdir.getName(), HashFunctions.getFileChecksum("SHA-256", subdir));
                 try {
-                    writer = new FileWriter("C:\\Users\\Farhan\\IdeaProjects\\anticheat-jar\\writing.txt", true);
-                    writer.write(subdir.getName() + "," + HashFunctions.getFileChecksum("SHA-256", subdir));
-                    writer.write("\n");
-                    writer.close();
+                    serializeData(subdir.getName(),  HashFunctions.getFileChecksum("SHA-256", subdir));
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    public static void serializeData(String name, String hash) throws IOException{
+        FileWriter writer = new FileWriter("C:\\Users\\Farhan\\IdeaProjects\\anticheat-jar\\writing.txt", true);
+        writer.write(name + "," + hash);
+        writer.write("\n");
+        writer.close();
     }
 }
