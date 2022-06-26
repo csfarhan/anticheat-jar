a41863c9b277cfca0a8b4ee7c79caf9ecca0cc59
csfarhan <89839649+csfarhan@users.noreply.github.com>
2022-06-26 16:24:01 -0400

    Created a register page, login page, dashboard page. Functionality of buttons not fully implemented.

--- src/MainWrapper.java
+++ src/MainWrapper.java
@@ -11,10 +11,9 @@ public class MainWrapper {
                 "referenceTest.txt");
                 antiCheat.start();
          */
-        /*
          AntiCheatFrame antiCheatFrame = new AntiCheatFrame( "C:\\Users\\Farhan\\IdeaProjects\\anticheat-jar\\Nuntu","referenceTest.txt" );
-         antiCheatFrame.initWindow();
-         */
+         antiCheatFrame.NewWindow();
+
         DatabaseOperation testDatabase = DatabaseOperation.getInstance("jdbc:sqlserver://poromtest.mssql.somee.com;database=poromtest;user=PoromK_SQLLogin_1;password=prnclvbss7;encrypt=true;trustServerCertificate=true;loginTimeout=30;");
         //testDatabase.executeStatement("CREATE TABLE Auth (id INTEGER IDENTITY(1,1) not NULL PRIMARY KEY, email VARCHAR(MAX) not NULL,  hash VARCHAR(MAX) not NULL)");
         Scanner in = new Scanner(System.in);
@@ -22,7 +21,7 @@ public class MainWrapper {
         System.out.println("1 to register test data");
         System.out.println("2 to login test data");
 
-        if(in.next().equals("1")){
+        /*if(in.next().equals("1")){
             User.registerUser("Porom.Kamal@gmail.com", "@Porom2002", testDatabase); // Already registered don't call this again
         }else{
             User.loginUser("PoromKamal@gmail.com", "@Porom200222", testDatabase); //incorrect login test
@@ -35,5 +34,7 @@ public class MainWrapper {
 
             testDatabase.close();
         }
+         */
     }
+
 }
